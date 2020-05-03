a9867c01cc1bcd95b35719fc010130ac97fcba1b
Hua Yang <dlyhua@cn.ibm.com>
2020-05-03 13:03:40 +0800

    first commit

---
+++ src/main/java/com/yh/SpringbootMabatisDemoApplication.java
@@ -0,0 +1,13 @@
+package com.yh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMabatisDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMabatisDemoApplication.class, args);
+	}
+
+}
